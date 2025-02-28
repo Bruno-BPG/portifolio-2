68a3cbefd6b35c597c6e7fff2f64a3897fd1e0bc
Bruno-BPG <gomesbruno030@outlook.com>
2025-02-28 03:28:46 -0300

    2

--- src/components/SkillsBar/index.jsx
+++ src/components/SkillsBar/index.jsx
@@ -19,19 +19,19 @@ import { dataBase } from "../../dabaBase";
 // import { AiFillGithub } from "react-icons/ai";
 
 export function SkillsBar() {
-  const [titleTech, setTitleTech] = useState("");
-  // const [techInfo, setTechInfo] = useState("");
-  const [techMyInfo, setTechMyInfo] = useState("");
+  const [visivel, setVisivel] = useState(false);
+  const [tecnologiaName, setTecnologiaName] = useState("");
 
-  function ShowsInformationAbouttheTechnology(title) {
-    let data = dataBase.find((element) => element.title === title);
+  function ShowsVisibility(visibility, dado) {
+    setTecnologiaName(dado)
 
-    setTitleTech(data.title);
-    // setTechInfo(data.techInfo);
-    setTechMyInfo(data.myTechInfo);
+    console.log(visivel, tecnologiaName)
+    console.log(visivel, tecnologiaName)
 
-    // console.log(techInfo)
-    // console.log(techInfo2)
+
+    setVisivel(visibility)
+
+
   }
 
   return (
@@ -42,75 +42,104 @@ export function SkillsBar() {
 
       <div className="internalBoxIcon">
         <div>
+
           <div className="boxIcon">
             <p
-              onClick={() => {
-                ShowsInformationAbouttheTechnology("HTML 5");
-              }}
+              onMouseEnter={() => ShowsVisibility(true, "HTML")}
+              onMouseLeave={() => ShowsVisibility(false)}
               className="icon"
             >
-              <FaHtml5 />
+              {visivel == true && tecnologiaName == "HTML" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+
+              <p className="iconTech"><FaHtml5 /></p>
             </p>
+
             <p
-              onClick={() => {
-                ShowsInformationAbouttheTechnology("CSS 3");
-              }}
+              onMouseEnter={() => ShowsVisibility(true, "CSS")}
+              onMouseLeave={() => ShowsVisibility(false)}
               className="icon"
             >
-              <FaCss3Alt />
+              {visivel == true && tecnologiaName == "CSS" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><FaCss3Alt /></p>
+
             </p>
+
             <p
-              onClick={() => {
-                ShowsInformationAbouttheTechnology("JavaScript");
-              }}
+              onMouseEnter={() => ShowsVisibility(true, "JavaScript")}
+              onMouseLeave={() => ShowsVisibility(false)}
               className="icon"
             >
-              <FaJs />
+              {visivel == true && tecnologiaName == "JavaScript" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><FaJs /></p>
             </p>
+
             <p
-              onClick={() => {
-                ShowsInformationAbouttheTechnology("Node.js");
-              }}
+              onMouseEnter={() => ShowsVisibility(true, "NodeJS")}
+              onMouseLeave={() => ShowsVisibility(false)}
               className="icon"
             >
-              <FaNode />
+              {visivel == true && tecnologiaName == "NodeJS" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><FaNode /></p>
             </p>
+
             <p
-              onClick={() => {
-                ShowsInformationAbouttheTechnology("React");
-              }}
+              onMouseEnter={() => ShowsVisibility(true, "React")}
+              onMouseLeave={() => ShowsVisibility(false)}
               className="icon"
             >
-              <FaReact />
+              {visivel == true && tecnologiaName == "React" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><FaReact /></p>
             </p>
-            <p className="icon">
-              <SiPostgresql />
+
+            <p
+              onMouseEnter={() => ShowsVisibility(true, "Nextjs")}
+              onMouseLeave={() => ShowsVisibility(false)}
+              className="icon"
+            >
+              {visivel == true && tecnologiaName == "Nextjs" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><TbBrandNextjs /></p>
             </p>
-            <p className="icon">
-              <SiTypescript />
+
+            <p
+              onMouseEnter={() => ShowsVisibility(true, "Postgres")}
+              onMouseLeave={() => ShowsVisibility(false)}
+              className="icon"
+            >
+              {visivel == true && tecnologiaName == "Postgres" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><SiPostgresql /></p>
             </p>
-            <p className="icon">
-              <FaGit />
+
+            <p
+              onMouseEnter={() => ShowsVisibility(true, "TypeScript")}
+              onMouseLeave={() => ShowsVisibility(false)}
+              className="icon"
+            >
+              {visivel == true && tecnologiaName == "TypeScript" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><SiTypescript /></p>
             </p>
-            <p className="icon">
-              <TbBrandNextjs />
+
+            <p
+              onMouseEnter={() => ShowsVisibility(true, "Python")}
+              onMouseLeave={() => ShowsVisibility(false)}
+              className="icon"
+            >
+              {visivel == true && tecnologiaName == "Python" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><FaPython /></p>
             </p>
-            <p className="icon">
-              <FaPython />
+
+            <p
+              onMouseEnter={() => ShowsVisibility(true, "Git")}
+              onMouseLeave={() => ShowsVisibility(false)}
+              className="icon"
+            >
+              {visivel == true && tecnologiaName == "Git" && <p className="msmTechSpanBox"><p className="msmTechSpan">{tecnologiaName}</p></p>}
+              <p className="iconTech"><FaGit /></p>
             </p>
-          </div>
-        </div>
-        <div className="boxInformation">
-          <div className="boxInformationMedium">
-            <div className="boxInformationTop">
-              <h1>{titleTech}</h1>
-            </div>
-            <div className="boxInformationLow">
-              <p>{techMyInfo}</p>
-            </div>
           </div>
         </div>
       </div>
+
+
     </StyleSkillsBar>
   );
 }
--- src/components/SkillsBar/style.jsx
+++ src/components/SkillsBar/style.jsx
@@ -2,10 +2,52 @@ import styled from "styled-components";
 
 export const StyleSkillsBar = styled.header`
     background-color: #998496;
-    /* background-color: greenyellow; */
 
-    width: 100%;
-    height: 500px;
+    /* width: 100%; */
+    /* height: 500px; */
+    
+    padding-bottom: 20px;
+    padding-top: 20px;
+    
+    .icon{
+        font-size: 125px;
+        padding: 8px;
+        /* background-color: red; */
+        margin: 11px;
+    }
+
+    .msmTechSpanBox{
+        /* width: 50px;
+        height: 50px;
+        border-radius: 15px 15px 15px 15px; */
+        background-color: aliceblue;
+        /* font-size: 20px; */
+        position: absolute;
+        /* margin-left: 50px; */
+        /* padding: 10px; */
+        /* margin-bottom: 100px; */
+    }
+
+    .msmTechSpan{
+        margin-top: -160px;
+        /* margin-left: 40px; */
+        border-radius: 15px 15px 15px 15px; 
+        font-size: 20px;
+        justify-content: center;
+        background-color: aliceblue;
+        color: black;
+        padding: 11px;
+        position: absolute;
+    }
+
+    .iconTech{
+        /* background-color: blue; */
+        cursor: pointer;
+        font-size: 150px;
+        margin: 10px;
+
+
+    }
 
     .titleOfSkillsBar1{
         /* background-color: green; */
@@ -16,69 +58,24 @@ export const StyleSkillsBar = styled.header`
 
     }
 
-    h1{
-        /* margin-bottom: -20px; */
-    }
-
     .boxIcon{
         display: flex;
-        justify-content: space-around;
-        width: 500px;
-        background-color: red;
+        width: 1000px;
+        /* background-color: red; */
         flex-wrap: wrap;
-        
     }
 
     .internalBoxIcon{
         display: flex;
         justify-content: center;
-        /* background-color: greenyellow; */
         background-color: #998496;
-        height: 600px;
         width: 100%;
     }
 
-    .boxInformation{
-        text-align: center;
-        background-color: #695a67;
-        height: 400px;
-        width: 600px;
-        margin-left: 150px;
-        /* display: flex;
-        justify-content: center;
-        align-items: center; */
 
-    }
 
-    .boxInformationMedium{
-        /* background-color: blue; */
-    }
-
-    .boxInformationTop{
-        display: flex;
-        justify-content: center;
-        /* background-color: #ccff33; */
-    }
-
-    .boxInformationLow{
-        /* background-color: purple; */
-        display: flex;
-        font-size: 20px;
-        justify-content: center;
-        padding: 20px;
-        
-    }
-
-    .icon{
-        font-size: 90px;
-        margin: 10px;
-        cursor: pointer;
-        /* color: aliceblue; */
-    }
-
-    .icon:hover{
-        color: #ccff33;
-        /* color: #998496; */
+    .iconTech:hover{
+        /* color: #ccff33; */
     }
 
 `
